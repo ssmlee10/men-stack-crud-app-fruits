5c9656c9cb470e9de6b4673d6e1b16a706574d46
Sharon Lee <ssmlee10@gmail.com>
2025-07-18 10:52:45 -0700

    define the schema and export the model

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
+// connects to mongodb fruits
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
+
+// Import the fruit model
+const Fruit = require('./models/fruit.js');
 
 // GET /
 app.get('/', async (req, res) => {
@@ -19,6 +23,12 @@ app.get('/', async (req, res) => {
     // render is taking a file, and show it on the browser
     // res.send, sends a very simple string
 });
+
+//GET /fruits/new
+// get the page that shows the user the form
+app.get('/fruits/new', (req, res) => {
+    res.send('This route sends the user to a form!');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');

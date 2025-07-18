edb9628169635dc23a1e8c4e1ca4911f393d9d02
Sharon Lee <ssmlee10@gmail.com>
2025-07-18 09:36:21 -0700

    create views, index.ejs

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 
 const app = express();
 
+// GET /
+app.get('/', async (req, res) => {
+    res.send('hello friend');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });

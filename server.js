b116383188f4e0f4c5080d7212974b92f354290f
Sharon Lee <ssmlee10@gmail.com>
2025-07-21 09:17:03 -0700

    add fruit index page

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
-const dotenv = require('dotenv');// bringing the functionality of dotenv
+const dotenv = require("dotenv"); // bringing the functionality of dotenv
 dotenv.config(); // using dotenv to bring the variables from the .env file
 
-const express = require('express'); 
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 
 // connects to mongodb fruits
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
 // Import the fruit model
-const Fruit = require('./models/fruit.js');
+const Fruit = require("./models/fruit.js");
 
 // adding middleware for app
 app.use(express.urlencoded({ extended: false }));
 
 // GET /
-app.get('/', async (req, res) => {
-    res.render('index.ejs');
-    // render helps us send our templates to the browser
-    // render is taking a file, and show it on the browser
-    // res.send, sends a very simple string
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+  // render helps us send our templates to the browser
+  // render is taking a file, and show it on the browser
+  // res.send, sends a very simple string
+});
+
+// define index
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find({});
+  // console.log(allFruits);
+  res.render("fruits/index.ejs", { fruits: allFruits });
 });
 
 // GET /fruits/new
 // get the page that shows the user the form
-app.get('/fruits/new', (req, res) => {
-    res.render('fruits/new.ejs');
-})
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
 
 // POST /fruits
-app.post('/fruits', async (req, res) => {
-    // req.body is what we are modifying
-    // console.log(req.body);
-    if (req.body.isReadyToEat === 'on') {
-        req.body.isReadyToEat = true;
-    } else {
-        req.body.isReadyToEat = false;
-    }
+app.post("/fruits", async (req, res) => {
+  // req.body is what we are modifying
+  // console.log(req.body);
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
 
-    // this line is the database transaction
-    await Fruit.create(req.body);
-    res.redirect('/fruits/new');
+  // this line is the database transaction
+  await Fruit.create(req.body);
+  res.redirect("/fruits");
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });

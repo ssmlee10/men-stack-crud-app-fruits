25ab5c9810e70fd369f3c08e50ffef16f972bd2a
Sharon Lee <ssmlee10@gmail.com>
2025-07-21 08:15:18 -0700

    complete fruit create functionality

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connection.on('connected', () => {
 // Import the fruit model
 const Fruit = require('./models/fruit.js');
 
+// adding middleware for app
+app.use(express.urlencoded({ extended: false }));
+
 // GET /
 app.get('/', async (req, res) => {
     res.render('index.ejs');
@@ -24,11 +27,26 @@ app.get('/', async (req, res) => {
     // res.send, sends a very simple string
 });
 
-//GET /fruits/new
+// GET /fruits/new
 // get the page that shows the user the form
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs');
 })
+
+// POST /fruits
+app.post('/fruits', async (req, res) => {
+    // req.body is what we are modifying
+    // console.log(req.body);
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    // this line is the database transaction
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
